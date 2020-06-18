70e16c54a543e8279d86bd71980ce0810bd15a14
ZQakane <zhenq021@gmail.com>
2020-06-18 18:32:17 +0800

    deploy

---
+++ precache-manifest.a3fca66b341c7a01783d3f1215e95947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2dac91ee68f53bcb7e",
+    "url": "/CopperDaily-website/css/app.b05c7357.css"
+  },
+  {
+    "revision": "774a83c303c8cb295ae225703c1e0f92",
+    "url": "/CopperDaily-website/index.html"
+  },
+  {
+    "revision": "9d2dac91ee68f53bcb7e",
+    "url": "/CopperDaily-website/js/app.fd3b5c26.js"
+  },
+  {
+    "revision": "5f531c36baeae3227418",
+    "url": "/CopperDaily-website/js/chunk-vendors.49df91d5.js"
+  },
+  {
+    "revision": "4e775552f31831da336df2469916bf8d",
+    "url": "/CopperDaily-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/CopperDaily-website/robots.txt"
+  }
+]);
